f7f0b5560b581dad1c859331d48fca791a15c87b
Tri <trimpham98@gmail.com>
2022-12-03 00:00:01 -0800

    Add comments for print statement and user input

--- go/playground.go
+++ go/playground.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// Print statement
 	fmt.Println("Hello world")
 
+	// User input
 	var userName string
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&userName)
