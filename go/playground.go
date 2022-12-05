7bcda4ac5e7d680cba2fb943e3cbc1a6c1d2ad29
Tri <trimpham98@gmail.com>
2022-12-04 21:53:43 -0800

    Add function definition for printGrade

--- go/playground.go
+++ go/playground.go
@@ -2,18 +2,38 @@ package main
 
 import "fmt"
 
+func printGrade(score float64) string {
+	switch {
+	case score >= 90:
+		return "A"
+	case score >= 80:
+		return "B"
+	case score >= 70:
+		return "C"
+	case score >= 60:
+		return "D"
+	default:
+		return "F"
+	}
+}
+
 func main() {
 	// Print statement
 	fmt.Println("Hello world")
 
 	// User input
 	var userName string
+	var userScore float64
 	fmt.Print("Enter your name: ")
 	fmt.Scan(&userName)
 	fmt.Print("Your name is ", userName)
 
+	fmt.Print("\nEnter your score: ")
+	fmt.Scan(&userScore)
+	fmt.Print("With a score of ", userScore, " you earned a grade of ", printGrade(userScore))
+
 	// for loop
-	for i := 1; i <= 500; i++ {
+	for i := 1; i <= 5; i++ {
 		fmt.Println(i)
 	}
 }
