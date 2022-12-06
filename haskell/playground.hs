bd5d2270727440648fd5094824ff53982d35aa5c
Tri <trimpham98@gmail.com>
2022-12-06 00:25:17 -0800

    Add function with guards

--- haskell/playground.hs
+++ haskell/playground.hs
@@ -1,2 +1,13 @@
+grade :: Int -> String
+grade score
+  | score >= 90 = "A"
+  | score >= 80 = "B"
+  | score >= 70 = "C"
+  | score >= 60 = "D"
+  | otherwise = "F"
+
+main :: IO ()
 main = do
-    putStrLn "Hello world!"
+  putStrLn "Enter your score: "
+  score <- readLn
+  putStrLn ("Your grade is: " ++ (grade score))
